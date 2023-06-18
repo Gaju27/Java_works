f4581887669f1559897b7ab7ec55dc8e99f34e32
Gajanana Ganjigatti <40160330+Gaju27@users.noreply.github.com>
2023-06-18 10:37:14 +0530

    Add files via upload

--- CountDigitsInNumber.java
+++ CountDigitsInNumber.java
@@ -1,17 +1,29 @@
 package PepCoding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class CountDigitsInNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
+        ArrayList<Integer> result = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
         int count=0;
         while(n!=0){
+            builder.append(n%10);
+            result.add(n%10);
             n=n/10;
             count++;
         }
 
+        System.out.println(result);
+        Collections.sort(result);
+
         System.out.println(count);
+        System.out.println(result);
+
+        System.out.println(builder.toString());
     }
 }
